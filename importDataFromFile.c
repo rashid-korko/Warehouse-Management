c21d1cdae57da08d8057d381e4353ecc0293220c
Rashid Korko <benooo.sy.96@gmail.com>
2023-01-26 01:11:16 +0330

    update

--- importDataFromFile.c
+++ importDataFromFile.c
@@ -4,32 +4,24 @@
 
 extern struct AccountData user;
 extern account_list head_account;
-struct Node temprature;
 
-void PushAccountFromFile(account_list * head, struct AccountData user)
-{
-    account_list * current = head;
-    while (current->next != NULL)
-    {
+void PushAccountFromFile(account_list *head, struct AccountData user) {
+    account_list *current = head;
+    while (current->next != NULL) {
         current = current->next;
     }
     current->next = (account_list *) malloc(sizeof(account_list));
-    head_account.user = user;
+    current->next->user = user;
     current->next->next = NULL;
 }
 
 
 
-void WriteAccountsInFile(account_list * head)
+void WriteAccountsInFile(account_list *head)
 {
     FILE *AccountDataFile;
     AccountDataFile = fopen("AccountData.txt" , "w");
-    if (AccountDataFile == NULL)
-    {
-        printf("ERROR , File could not be opened");
-        return;
-    }
-    account_list * current = head;
+    account_list *current = head;
     while (current != NULL) {
         printf("%s\n", current->user.UserName);
         fwrite(&current->user , sizeof(struct AccountData) , 1 , AccountDataFile);
@@ -48,7 +40,6 @@ void ImportDataFromFile()
    {
         head_account.user = user;
         head_account.next = NULL;
-        checked_end_of_file = fread(&user , sizeof(struct AccountData) , 1 , AccountDataFile);
    }
    while (checked_end_of_file)
    {
